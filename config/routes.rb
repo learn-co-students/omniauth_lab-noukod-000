71b9135f4c0bdb72e6931da28669bd246a12fde7
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-01 17:18:00 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-
-  
+  get '/auth/facebook' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -29,7 +28,7 @@ Rails.application.routes.draw do
   #     end
   #   end
 
-  # Example resource route with sub-resources:
+  # Example resource route with sub-resources
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
